0d7a09a98a59b140bf7fc0955c96613f6d47ec75
Diana Calvache <calvache.diana@gmail.com>
2016-04-02 17:41:42 -0500

    Add seed volunteers and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def select_random_user_id
+  User.all.sample.id
+end
+
+address = Address.create(street_address_1: "123 Fake St", city: "Springfield", state: "IL", zip_code: "60607")
+
+10.times do |n|
+  User.create(first_name: "Jane", last_name: "Doe", phone_number: "1231231234", role: "volunteer", address_id:  address.id, email: "test#{n}@example.com", password: "password", password_confirmation: "password")
+end
+
+10.times do |n|
+  Volunteer.create(gender: "F", user_id: select_random_user_id)
+end
