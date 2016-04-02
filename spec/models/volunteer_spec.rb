bb7767c79412ad465411e8bbd0dfdbfced77ac94
Diana Calvache <calvache.diana@gmail.com>
2016-04-02 10:39:30 -0500

    Rename fixtures and add missing values

--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Volunteer do
 
   it { should belong_to(:user) }
-  it { should have_one(:car) }
+  it { should have_one(:vehicle) }
   it { should have_one(:accommodation) }
 
 end
