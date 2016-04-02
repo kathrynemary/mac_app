7e3f01a35a8295c9231aa5492043d2b50242d92c
Kate Pham <tpham@civisanalytics.com>
2016-04-02 14:44:21 -0500

    Add edit, new, index, show views for volunteers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
   has_one :volunteer
   has_one :organizer
   belongs_to :address
+
+  accepts_nested_attributes_for :address
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+
+  get 'volunteers' => redirect('volunteers/index')
   get 'volunteers/index'
 
   get 'home/index'
