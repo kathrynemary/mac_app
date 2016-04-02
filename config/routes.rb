1685c8b24e60708575639b455feac23000e120c1
Eileen McFarland <eileenm@gmail.com>
2016-04-02 11:11:39 -0500

    set up devise authentication for user :) :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'volunteers/index'
 
   get 'home/index'
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
