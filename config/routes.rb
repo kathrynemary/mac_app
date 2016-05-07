3df352340bac49fe00a120cef562537e309dd8f5
Nicole Lopez <nicole.lopez@avant.com>
2016-05-07 13:58:09 -0500

    changed namespacing of rails admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/data_admin', as: 'rails_admin'
   devise_for :users
 
   get 'volunteers' => redirect('volunteers/index')
