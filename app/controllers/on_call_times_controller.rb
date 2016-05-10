2519d9d7fa3bab4f0f57149adf64d58d1a819d59
CorainChicago <chaysmagan@gmail.com>
2016-05-09 22:24:23 -0500

    move display of call times into modal

--- app/controllers/on_call_times_controller.rb
+++ app/controllers/on_call_times_controller.rb
@@ -5,7 +5,23 @@ class OnCallTimesController < ApplicationController
   end
 
   def new
-  
-  end  
+    @call_times = OnCallTime.new
+  end 
+
+  def create
+    @call_time = OnCallTime.new(params[:call_times])
+
+    if @call_times.save?
+      render
+
+
+  end 
+
+  def destroy
+    @call_time = OnCallTime.find(params[:id])
+    @call_time.destroy
+
+    redirect_to on_call_times_path
+  end
 end
 
