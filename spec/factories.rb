e938048cf5b92cdfe302d79956f2b7e94e2fc8af
CorainChicago <chaysmagan@gmail.com>
2016-08-01 20:21:26 -0500

    update attributes for accommodation's factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
    factory :accommodation do
-    bedding "Through the Looking Glass"
+    bedding "queen sized bed in guest bedroom"
     privacy "locked door"
-    guest_capacity 2
-    pet "two cats"
+    guest_capacity 1
+    pet "none"
     num_roommates 2
     volunteer_id 1
   end
