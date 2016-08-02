33b15e620d742f2b895da5f7de12f98bf4d951e5
CorainChicago <chaysmagan@gmail.com>
2016-08-01 20:09:58 -0500

    update factories to only include accomidations

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-   factory :post do
-    title "Through the Looking Glass"
-    user
+
+   factory :accommodation do
+    bedding "Through the Looking Glass"
+    privacy "locked door"
+    guest_capacity 2
+    pet "two cats"
+    num_roommates 2
+    volunteer_id 1
   end
 end
