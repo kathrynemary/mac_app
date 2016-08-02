786b951d8c4a940978daf7f4f119c0b3cb0523a9
Kathryne Dunlap <kathrynemary@gmail.com>
2016-08-02 08:55:57 -0500

    rails fxn'l but getting weird error re: "@volunteers.empty?"

--- spec/views/volunteers/index.html.haml_spec.rb
+++ spec/views/volunteers/index.html.haml_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "volunteers/index.html.haml", type: :view do
+ # before :each do
+ #   User.create(first_name: "Jane", last_name: "Doe")
+ # end
+
   it "does math" do
     expect(1 + 1).to eq(2)
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "displays some text" do
+    render :text => "This is rendered!"
+    expect(rendered).to include("rendered")
+  end
+
+  it "displays the new volunteer button" do
+    render
+    expect(rendered).to have_link("New Volunteer Account", new_volunteer_path)
+  end
+
 end
