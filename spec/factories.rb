457ac2e3e3afb8652038c7e8d842e916f5ec9895
CorainChicago <chaysmagan@gmail.com>
2016-08-01 22:44:29 -0500

    create a factory for addresses

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,7 +5,16 @@ FactoryGirl.define do
     privacy "locked door"
     guest_capacity 1
     pet "none"
-    num_roommates 2
+    num_roommates 1
     volunteer_id 1
   end
+
+  factory :address do 
+    street_address_1 Faker::Address.street_address
+    street_address_2 Faker::Address.secondary_address
+    city Faker::Address.city
+    state Faker::Address.state
+    zip_code Faker::Address.zip
+  end
+
 end
