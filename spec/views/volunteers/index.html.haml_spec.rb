65757be097e1006c70a2a440658b413be5aab26f
Kathryne Dunlap <kathrynemary@gmail.com>
2016-08-01 20:14:49 -0500

    rspec is functioning

---
+++ spec/views/volunteers/index.html.haml_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "volunteers/index.html.haml", type: :view do
+  it "does math" do
+    expect(1 + 1).to eq(2)
+  end
+
+  pending "add some examples to (or delete) #{__FILE__}"
+end
