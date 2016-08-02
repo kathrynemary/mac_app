095679ad62642d90fb5c91eef2b8898e2fc663f6
CorainChicago <chaysmagan@gmail.com>
2016-08-01 19:53:45 -0500

    add test about creating a valid factory for accomidation

--- spec/models/accommodation_spec.rb
+++ spec/models/accommodation_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe Accommodation do
+  subject { build(:accommodation) }
 
   it { should belong_to(:volunteer) }
 
+  it "has a valid factory" do
+    subject.valid?
+    expect(subject.errors.messages).to eq({})
+  end
 end
