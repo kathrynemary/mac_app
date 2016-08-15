3b3e305dd7939e15fb81935a4167b0c4adfd6f02
Nicole Lopez <nicole.lopez@avant.com>
2016-08-14 22:22:19 -0500

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def select_random_user_id
-  User.all.sample.id
-end
-
-address = Address.create(street_address_1: "123 Fake St", city: "Springfield", state: "IL", zip_code: "60607")
-
-10.times do |n|
-  User.create(first_name: "Jane", last_name: "Doe", phone_number: "1231231234", role: "volunteer", address_id:  address.id, email: "test#{n}@example.com", password: "password", password_confirmation: "password")
+  @u ||= User.order("RANDOM()").pluck(:id).to_a.dup
 end
 
 10.times do |n|
-  Volunteer.create(gender: "F", user_id: select_random_user_id)
+  User.create(first_name: "Jane", last_name: "Doe", phone_number: "1231231234", role: "volunteer", email: "test#{n}@example.com", password: "password", password_confirmation: "password")
+end
+
+User.all.each do |u|
+  Address.create(street_address_1: "123 Fake St", city: "Springfield", state: "IL", zip_code: "60607", user: u)
+end
+
+5.times do |n|
+  Volunteer.create(gender: "F", user_id: select_random_user_id.pop)
+end
+
+5.times do |n|
+  Organizer.create(position: "position", board_member: true, user_id: select_random_user_id.pop)
 end
