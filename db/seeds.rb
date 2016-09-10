b92a260372b23c0859e86555c22021b46513922b
Nicole Lopez <nicole.lopez@avant.com>
2016-09-10 16:32:31 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def select_random_user_id
-  @u ||= User.order("RANDOM()").pluck(:id).to_a.dup
+def board_positions
+  [ "Executive Director",
+    "Director of Operations",
+    "Director of Communications",
+    "Director of Finance",
+    "Director of Outreach",
+    "Director of Technology"
+  ]
 end
 
-10.times do |n|
-  User.create(first_name: "Jane", last_name: "Doe", phone_number: "1231231234", role: "volunteer", email: "test#{n}@example.com", password: "password", password_confirmation: "password")
+def volunteers
+  [
+    "driver",
+    "host",
+    "childcare"
+  ]
+end
+
+board_positions.each_with_index do |position, n|
+  user = User.create(first_name: "Jane#{n}", last_name: "Doe#{n}", phone_number: "1231231234", role: "board_member", email: "test#{n}@example.com", password: "password", password_confirmation: "password")
+  Organizer.create(position: position, board_member: true, user_id: user.id)
+end
+
+volunteers.each_with_index do |capacity, n|
+  user = User.create(first_name: "Wanda#{n}", last_name: "Starling#{n}", phone_number: "1231231234", role: capacity, email: "test#{n}@examplevol.com", password: "password", password_confirmation: "password")
+  Volunteer.create(gender: "F", user_id: user.id)
 end
 
 User.all.each do |u|
   Address.create(street_address_1: "123 Fake St", city: "Springfield", state: "IL", zip_code: "60607", user: u)
-end
-
-5.times do |n|
-  Volunteer.create(gender: "F", user_id: select_random_user_id.pop)
-end
-
-5.times do |n|
-  Organizer.create(position: "position", board_member: true, user_id: select_random_user_id.pop)
 end
